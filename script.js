bdd83f1b8c29d4e5ca046acfcc25cfec10d8f2b8
ryan <ryanpburnett@yahoo.com>
2023-07-10 12:58:21 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const fibonacci = (iterations) => {
+    let out = [1];
+
+    for (let i = 1; i < iterations; i++) {
+        if (iterations === 1) {
+            return out;
+        }
+
+        if (out.length === 1) {
+            out.push(1);
+        }else{
+            out.push(out[(out.length - 1)] + out[out.length - 2]);
+        }
+    }
+
+    return out;
+}
+
+let mil = fibonacci(100);
+console.log(mil);
+
+// HTML
+let fib = document.getElementById('fib');
+
+let render = `
+<table>
+    <tr>
+        <th>iteration</th>
+        <th>value</th>
+    </tr>`;
+render += mil.map((item, index) => `
+<tr>
+    <td>${index + 1}</td>
+    <td>${item}</td>
+</tr>`).join('');
+render += `</table>`;
+
+fib.innerHTML = render;
